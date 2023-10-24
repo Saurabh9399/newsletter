6f75db17bbe9df7eb2aa23b22afc20147e4bb1b7
Saurabh Jirapure <64205497+Saurabh9399@users.noreply.github.com>
2023-10-24 17:59:10 +0530

    validation using formik and success

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Signup from './components/Signup';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SuccessPage from './components/SuccessPage';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Signup/>,
+    },
+    {
+      path: "/success",
+      element: <SuccessPage/>,
+    },
+  ]);
   return (
-    <div className="bg-gray-700 p-4">
-        <Signup/>
+    <div className="bg-gray-700 p-4 h-screen py-[4%]">
+         <RouterProvider router={router} /> 
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,19 +1,38 @@
 import React from "react";
-import ReactLogo from "../images/illustration-sign-up-desktop.svg"
-import { useFormik } from 'formik';
+import ReactLogo from "../images/illustration-sign-up-desktop.svg";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const navigate = useNavigate()
+  const validate = (values) => {
+    const errors = {};
+
+    if (!values.email) {
+      errors.email = "Required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues: {
-      email: '',
+      email: "",
     },
-    onSubmit: values => {
+    validate,
+    onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
+      navigate("success");
     },
   });
+
   return (
-    <div className="w-[55%] border border-gray-400 mx-auto bg-white rounded-2xl flex px-10 py-6 justify-center items-center">
-      <div className="w-1/2 border">
+    <div className="w-[55%] mx-auto bg-white rounded-2xl flex px-10 py-6 justify-center items-center">
+      <div className="w-1/2">
         <div className="text-4xl font-bold opacity-80">Stay Updated!</div>
         <p className="text-gray-700 mt-4 mb-4">
           Join 60,000+ product managers receiving monthly <br /> updates on:
@@ -33,7 +52,7 @@ const Signup = () => {
                 <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
                 <path
                   stroke="#FFF"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="M6 11.381 8.735 14 15 8"
                 />
               </g>
@@ -55,7 +74,7 @@ const Signup = () => {
                 <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
                 <path
                   stroke="#FFF"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="M6 11.381 8.735 14 15 8"
                 />
               </g>
@@ -77,33 +96,40 @@ const Signup = () => {
                 <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
                 <path
                   stroke="#FFF"
-                  stroke-width="2"
+                  strokeWidth="2"
                   d="M6 11.381 8.735 14 15 8"
                 />
               </g>
             </svg>
-            <span className="text-gray-600 text-xs">
-              And much more!
-            </span>
+            <span className="text-gray-600 text-xs">And much more!</span>
           </div>
         </div>
         <form className="flex flex-col" onSubmit={formik.handleSubmit}>
-        <label className="text-xs my-2 text-gray-700 font-bold">Email Address</label>
+          <label className="text-xs my-2 text-gray-700 font-bold">
+            Email Address
+          </label>
           <input
+            id="email"
+            name="email"
             placeholder="Enter email..."
             className="m-4 p-3 border border-gray-500 rounded ml-0 mt-0 w-[93%]"
             onChange={formik.handleChange}
             value={formik.values.email}
           />
           <button
+            type="submit"
             className="bg-red-500 w-[93%] mx-auto text-white rounded-lg p-2 ml-0 opacity-90 hover:opacity-80 font-bold text-sm"
-            onClick={(e) => e.preventDefault()}
           >
             Subscribe to monthly newsletter
           </button>
         </form>
+        {formik.errors.email ? (
+          <div className="text-red-600 m-4">{formik.errors.email}</div>
+        ) : null}
       </div>
-      <div className="border"><img src={ReactLogo} className="w-[100%] mx-auto"/></div>
+      <div className="">
+        <img src={ReactLogo} className="w-[100%] mx-auto" alt="background" />
+      </div>
     </div>
   );
 };
